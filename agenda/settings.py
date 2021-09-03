40ddab568381475327bf2ae5e62411d0336dfb66
Olavo-DIO <olavo.melo.br.DIO@gmail.com>
2021-09-03 14:53:07 -0300

    APP - Agenda de Contatos Commit inicial

--- agenda/settings.py
+++ agenda/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIR = [(BASE_DIR) / 'templates/static']
 STATICFILES_DIRS = [(BASE_DIR) / 'templates/static']
 
 # Default primary key field type
