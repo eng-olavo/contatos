134c5d9f6ef736e981e355a6b6d5901f2d99f331
Olavo-DIO <olavo.melo.br.DIO@gmail.com>
2021-09-03 15:32:44 -0300

    APP - Agenda de Contatos Commit 3 configurando o django-admin

--- contatos/models.py
+++ contatos/models.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
 
 class Contato(models.Model):
     nome = models.CharField(max_length=150)
@@ -15,3 +17,5 @@ class Contato(models.Model):
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
 
+    def __str__(self):
+        return self.nome
